6f81635f47f8fab16b02452b778601ef3ab38fef
Vladislav Belogrudov <v.belogrudov@yadro.com>
2022-09-15 15:40:50 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vlad-belogrudov/greetings
+
+go 1.19
---
+++ greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+// TestHelloName calls greetings.Hello with a name
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// testHelloEmpty calls greeting.Hello with an empty string
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
